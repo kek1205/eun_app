cf26486f2038fe9ce2b4d0fb274eec744abce93e
eun <e.keum1205@gmail.com>
2011-10-29 14:12:08 -0500

    firt test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EunApp::Application.config.secret_token = 'd5bade7f6f8eec6de173d7a65228bd3914dfde271850ab78bbad0a1e7250461eeb755efa7f74fc9b72574e92f550976e17798f7152337034287b14f39314c254'
